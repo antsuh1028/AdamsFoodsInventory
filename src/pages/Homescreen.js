a724c7d8c4705cefbef624fa72d2a95a73adaa68
Anthony Suh <anthods1@uci.edu>
2024-09-03 16:06:32 -0700

    made layout changes to homescreen

--- src/pages/Homescreen.js
+++ src/pages/Homescreen.js
@@ -1,28 +1,45 @@
-import { Select, Textarea, Box, VStack, Flex, Button, FormControl, FormLabel } from '@chakra-ui/react';
+import { Select, HStack, Textarea, Box, Input, VStack, Flex, Button, FormControl, FormLabel } from '@chakra-ui/react';
 import React, { useState } from 'react';
 import axios from 'axios'; // Ensure axios is imported for HTTP requests
 import Navbar from './Navbar';
 
 const Homescreen = () => {
-  const [inputs, setInputs] = useState(Array(13).fill(''));
+  // const [inputs, setInputs] = useState(Array(13).fill(''));
+  const [location, setLocation] = useState('');
+  const [lot, setLot] = useState('');
+  const [vendor, setVendor] = useState('');
+  const [brand, setBrand] = useState('');
+  const [species, setSpecies] = useState('');
+  const [description, setDescription] = useState('');
+  const [grade, setGrade] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [weight, setWeight] = useState('');
+  const [packdate, setPackdate] = useState('');
+  const [temp, setTemp] = useState('');
+  const [est, setEst] = useState('');
   const [showTextarea, setShowTextarea] = useState(false);
   const [textareaContent, setTextareaContent] = useState('');
 
-  const handleInputChange = (index, value) => {
-    const newInputs = [...inputs];
-    newInputs[index] = value;
-    setInputs(newInputs);
-  };
+  const inputs = {location,
+    lot,
+    vendor,
+    brand,
+    species,
+    description,
+    grade,
+    quantity,
+    weight,
+    packdate,
+    temp,
+    est}
 
   const handleFind = () => {
 
     axios.post('http://localhost:3001/inventoryFind', { inputs }) // Assuming you have an endpoint for submission
       .then(result => {
-        console.log(result);
-        if(result.data === "Success"){
-          setTextareaContent(inputs.map((input, index) => `Input ${index + 1}: ${input}`).join('\n'));
+        console.log("result:",result.data[0].location);
+        setTextareaContent(result.data[1].location, result.data[1].location);
           setShowTextarea(true);
-        }
       })
       .catch(err => console.log(err));
 
@@ -63,27 +80,83 @@ const Homescreen = () => {
           <form onSubmit={handleUpdateSubmit}>
             <FormControl>
               <VStack spacing={2} alignItems="center">
-                {Array.from({ length: 13 }, (_, index) => (
-                  <Box width="90%" key={index}>
-                    <FormLabel marginTop="5px" marginBottom="5px">{`Input ${index + 1}`}</FormLabel>
-                    <Select
-                      bg="white"
-                      width="100%"
-                      placeholder={`Select option for Input ${index + 1}`}
-                      onChange={(e) => handleInputChange(index, e.target.value)}
-                    >
-                      <option value="Option 1">Option 1</option>
-                      <option value="Option 2">Option 2</option>
-                      <option value="Option 3">Option 3</option>
-                    </Select>
-                  </Box>
-                ))}
+              
+                <HStack spacing={2} width="100%">
+                  <VStack spacing={1} width="75%">
+                    <FormLabel marginTop="5px" textAlign='left' marginBottom="5px">Location</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Location' onChange={(e) => setLocation(e.target.value)} />
+                  </VStack>
+                  <VStack spacing={1} width="25%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Lot</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Lot' onChange={(e) => setLot(e.target.value)} />
+                  </VStack>
+                </HStack>
+
+                
+
+                <HStack spacing={2} width="100%" marginTop="10px"> 
+                  </HStack>
+
+                <HStack spacing={2} width="100%">
+                  <VStack spacing={1} width="33%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Vendor</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Vendor' onChange={(e) => setVendor(e.target.value)} />
+                  </VStack>
+                  <VStack spacing={1} width="33%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Brand</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Brand' onChange={(e) => setBrand(e.target.value)} />
+                  </VStack>
+                  <VStack spacing={1} width="33%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Species</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Species' onChange={(e) => setSpecies(e.target.value)} />
+                  </VStack>
+                  
+                </HStack>
+
+                <FormLabel marginTop="5px" marginBottom="5px">Description</FormLabel>
+                <Input type='text' bg="white" width="100%" size="lg"  placeholder='Enter Description' onChange={(e) => setDescription(e.target.value)} />
+
+                <HStack spacing={4} width="100%">
+                  <VStack spacing={1} width="32%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Grade</FormLabel>
+                    <Input type='text' bg="white" width="100%" placeholder='Enter Grade' onChange={(e) => setGrade(e.target.value)} />
+                  </VStack>
+                  <VStack spacing={1} width="32%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Quantity</FormLabel>
+                    <Input type='number' bg="white" width="100%" placeholder='Enter Quantity' onChange={(e) => setQuantity(e.target.value)} />
+                  </VStack>
+                  <VStack spacing={1} width="32%">
+                    <FormLabel marginTop="5px" marginBottom="5px">Weight</FormLabel>
+                    <Input type='number' bg="white" width="100%" placeholder='Enter Weight' onChange={(e) => setWeight(e.target.value)} />
+                  </VStack>
+              </HStack>
+
+              <HStack spacing={4} width="100%">
+                <VStack spacing={1} width="32%">
+                  <FormLabel marginTop="5px" marginBottom="5px">Pack Date</FormLabel>
+                  <Input type='date' bg="white" width="100%" onChange={(e) => setPackdate(e.target.value)} />
+                </VStack>
+                <VStack spacing={1} width="32%">
+                  <FormLabel marginTop="5px" marginBottom="5px">Temperature</FormLabel>
+                  <Input type='number' bg="white" width="100%" placeholder='Enter Temperature' onChange={(e) => setTemp(e.target.value)} />
+                </VStack>
+                <VStack spacing={1} width="32%">
+                  <FormLabel marginTop="5px" marginBottom="5px">EST#</FormLabel>
+                  <Input type='text' bg="white" width="100%" placeholder='Enter Est' onChange={(e) => setEst(e.target.value)} />
+               </VStack>
+              </HStack>
+
+                
+                
+                
+                
               </VStack>
             </FormControl>
 
             <Flex alignItems="center" mt="20px">
+              <Button bg="white" margin="20px" >Add</Button>
               <Button bg="white" margin="20px" onClick={handleFind}>Find</Button>
-              <Button bg="white" margin="20px" type="submit">Update/Submit</Button>
+              <Button bg="white" margin="20px" type="submit">Update</Button>
               <Button bg="white" margin="20px" onClick={handleDelete}>Delete</Button>
             </Flex>
           </form>
